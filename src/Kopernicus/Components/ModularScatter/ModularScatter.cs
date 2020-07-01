5440c44a42f408373d9a6032adcdc32d6d0c43a9
Raymond Beehler <21GunSoftware@comcast.net>
2020-06-30 21:34:45 -0700

    Fix a dumb dumb.

--- src/Kopernicus/Components/ModularScatter/ModularScatter.cs
+++ src/Kopernicus/Components/ModularScatter/ModularScatter.cs
@@ -248,7 +248,7 @@ namespace Kopernicus.Components.ModularScatter
             for (Int32 i = 0; i < scatterObjects.Count; i++)
             {
                 if (scatterObjects[i])
-                    if (scatterObjects[i]) continue;
+                    if (scatterObjects[i])
                 {
                     //if (scatterObjects[i].transform.parent.name == "Unass")
                     //{
